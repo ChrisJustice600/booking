fb81b50ffb0be9f38017bcfdc631cbf2fce54095
unknown <christianmabeladi@gmail.com>
2023-10-03 04:38:06 +0200

    create tomorrow function for input end

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ start.value = today;
 // créons une fonction pour ajouter la date plus 1 jour à l'input End
 const tomorrow = ()=>{
     const day = new Date(today)
+    // console.log(day);
+    day.setDate(day.getDate() + 1)
+    let tomorrow = day.toISOString().split("T")[0];
     console.log(day);
+    end.value = tomorrow
+
 }
 tomorrow()
