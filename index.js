d84188451c1f62955d1b6d450ca3b224fab31063
unknown <christianmabeladi@gmail.com>
2023-10-03 04:54:33 +0200

    event change input

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const today = new Date().toISOString().split("T")[0];
 
 // console.log(today);
 start.value = today;
+start.min = today;
 
 // créons une fonction pour ajouter la date plus 1 jour à l'input End
 const tomorrow = ()=>{
@@ -12,8 +13,21 @@ const tomorrow = ()=>{
     // console.log(day);
     day.setDate(day.getDate() + 1)
     let tomorrow = day.toISOString().split("T")[0];
-    console.log(day);
+    // console.log(day);
     end.value = tomorrow
-
+    end.min = tomorrow
 }
 tomorrow()
+
+start.addEventListener("change", (e)=>{
+// console.log(e.target.value);
+    let day = new Date(e.target.value)
+    day.setDate(day.getDate() + 1)
+    let tomorrow = day.toISOString().split("T")[0];
+    console.log(day);
+    console.log(tomorrow);
+    end.value = tomorrow
+   
+
+ 
+})
